14541c0b7daa46e1c999ade6f2ce9b8519d74ee3
TomaszPietruszka <tpietruszka10@gmail.com>
2020-01-31 13:45:50 +0100

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import java.util.function.Supplier;
+
+public class Main {
+    public static void main(String[] args) {
+        TicTacToe ticTacToe = new TicTacToe();
+
+        ticTacToe.playGame();
+    }
+}
